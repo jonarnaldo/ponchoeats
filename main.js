005133ca479db5c435eb2517c60fa26a229a439c
jon arnaldo <jonarnaldo@gmail.com>
2017-10-01 23:05:35 -0700

    updates twitter.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = require('http').createServer((req, res) => res.send('Ahoy!'));
 const PORT = process.env.PORT || 3000;
 const dotenv = require('dotenv').config()
-const Twitter = require('twitter-node-client').Twitter;
+// const Twitter = require('twitter-node-client').Twitter;
+const twitter = require('./twitter.js').twitter;
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
@@ -12,25 +13,14 @@ app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-// TODO - move into a module
+console.log(twitter.getUserTimeline)
+
 //Callback functions
 var error = function (err, response, body) {
-    console.log('ERROR [%s]', JSON.stringify(err));
+    console.log('error', err);
 };
 var success = function (data) {
-    console.log('Data [%s]', data);
+    console.log('data', data);
 };
-
-const config ={
-  "consumerKey": process.env.CONSUMER_KEY,
-  "consumerSecret": process.env.CONSUMER_SECRET,
-  "accessToken": process.env.ACCESS_TOKEN,
-  "accessTokenSecret": process.env.ACCESS_SECRET,
-  "callBackUrl": ""
-}
-
-console.log(config)
-const twitter = new Twitter(config);
-console.log(twitter.getUserTimeline)
 
 twitter.getUserTimeline({ screen_name: 'jonarnaldo', count: '10'}, error, success);
--- twitter.js
+++ twitter.js
@@ -1,13 +1,5 @@
 const Twitter = require('twitter-node-client').Twitter;
 
-//Callback functions
-var error = function (err, response, body) {
-    console.log('ERROR [%s]', err);
-};
-var success = function (data) {
-    console.log('Data [%s]', data);
-};
-
 const config ={
   "consumerKey": process.env.CONSUMER_KEY,
   "consumerSecret": process.env.CONSUMER_SECRET,
